f308d52683049422c880ad7de1f49701f3bf984e
Rusty Russell <rusty@rustcorp.com.au>
2012-11-22 12:30:25 +1030

    __UNIQUE_ID()
    Jan Beulich points out __COUNTER__ (gcc 4.3 and above), so let's use
    that to create unique ids.  This is better than __LINE__ which we use
    today, so provide a wrapper.
    
    Stanislaw Gruszka <sgruszka@redhat.com> reported that some module parameters
    start with a digit, so we need to prepend when we for the unique id.
    
    Change-Id: I9fc3ae5af29419bcccd14e80fdc6dffafd2860c3
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- include/linux/compiler-gcc4.h
+++ include/linux/compiler-gcc4.h
@@ -31,6 +31,8 @@
 
 #define __UNIQUE_ID(prefix) __PASTE(__PASTE(__UNIQUE_ID_, prefix), __COUNTER__)
 
+#define __UNIQUE_ID(prefix) __PASTE(__PASTE(__UNIQUE_ID_, prefix), __COUNTER__)
+
 /*
  * GCC 'asm goto' miscompiles certain code sequences:
  *
